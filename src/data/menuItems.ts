5858d44f6561f0d904c298c2ef399ece3bf88991
Mahdi Hasan Mahi <mh7266391@gmail.com>
2025-01-24 22:28:41 +0600

    initiated

---
+++ src/data/menuItems.ts
@@ -0,0 +1,15 @@
+const menuItems = [
+    {
+      name: 'About',
+      path: '/about',
+    },
+    {
+      name: 'Shop',
+      path: '/shop',
+    },
+    {
+      name: 'Cart',
+      path: '/cart',
+    },
+  ];
+  export default menuItems;
